7dbf096271e243b1792cfffff4f57d2834b0ebe6
Veronika <anotheronetwothree@yandex.ru>
2020-05-18 16:56:42 +0300

    fixed scroll, add social

--- public/script.js
+++ public/script.js
@@ -56,3 +56,12 @@ var swiper = new Swiper('.swiper-container', {
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 });
+
+$(document).ready(function(){
+  $('a[href^="#"], *[data-href^="#"]').on('click', function(e){
+      e.preventDefault();
+      var t = 1000;
+      var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+      $('html,body').stop().animate({ scrollTop: $(d).offset().top }, t);
+  });
+});
